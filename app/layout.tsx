c98df6a09f924e860bbb9c65648f7b021e67852a
Aetius Gular <aetiusgular@gmail.com>
2026-02-09 17:26:11 -0800

    fff white background
    background change from off white to full white to match with images

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={vremena.variable}>
-      <body className="bg-neutral-900 text-white selection:bg-pink-500 dark:bg-neutral-50 dark:text-black dark:selection:bg-teal-300 dark:selection:text-black">
+      <body className="bg-neutral-900 text-white selection:bg-pink-500 dark:bg-white dark:text-black dark:selection:bg-teal-300 dark:selection:text-black">
         <CartProvider cartPromise={cart}>
           <Navbar />
           <main>
